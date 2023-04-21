952174ec9103c7054f3bdd7f9bf30fd2ad2cb03a
Signeemmanuel <signeemmanuel28@gmail.com>
2023-04-21 02:45:08 -0700

    Update INSTALL_APPS and DATABASE in todo_list_project/settings.py file

--- todo_list/todo_list_project/todo_list_project/settings.py
+++ todo_list/todo_list_project/todo_list_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'todo_list',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'todo_list_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo_list_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
