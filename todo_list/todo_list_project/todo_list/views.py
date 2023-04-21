178bbfe90a790084fb1bd0fcb344b138a9c22bba
Signeemmanuel <signeemmanuel28@gmail.com>
2023-04-21 03:22:48 -0700

    Add TodoItemList and TodoItemDetails in view.py

--- todo_list/todo_list_project/todo_list/views.py
+++ todo_list/todo_list_project/todo_list/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import TodoItems
+from .serializers import TodoItemSerializer
 
 # Create your views here.
+class TodoItemList(generics.ListCreateAPIView):
+    queryset = TodoItems.objects.all()
+    serializer_class = TodoItemSerializer
+    
+
+class TodoItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TodoItems
+    serializer_class = TodoItemSerializer
