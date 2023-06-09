e730a0eec1397d88c147ed3a20db3acb10cf31be
Signeemmanuel <signeemmanuel28@gmail.com>
2023-06-09 13:18:04 +0100

    Add urls.py

---
+++ Blog/blog_backend/blog/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ArticleViewSet, UserViewSet
+
+router = DefaultRouter()
+router.register('articles', ArticleViewSet, basename='articles')
+router.register('users', UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
